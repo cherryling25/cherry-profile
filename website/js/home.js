9388fb0d5582eac959712e040f0ab73e7790ffd8
Cherry <cherryling25@outlook.com>
2019-12-02 17:57:40 +0800

    header,main

---
+++ website/js/home.js
@@ -0,0 +1,22 @@
+window.onload=function(){
+    var aboutMe =document.getElementById('aboutMe');
+    var ulDropdown =document.getElementById('ulDropdown');
+        aboutMe.onmouseover = function() {
+            ulDropdown.style.display='block';
+        }
+        aboutMe.onmouseout = function() {
+            ulDropdown.style.display='none';
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+}
