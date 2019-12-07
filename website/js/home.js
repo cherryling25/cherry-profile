405a7a796fc7b6f3468f336c58f5fad84c981a6f
Cherry <cherryling25@outlook.com>
2019-12-07 20:30:47 +0800

    test

--- website/js/home.js
+++ website/js/home.js
@@ -1,22 +1,11 @@
-window.onload=function(){
-    var aboutMe =document.getElementById('aboutMe');
-    var ulDropdown =document.getElementById('ulDropdown');
-        aboutMe.onmouseover = function() {
-            ulDropdown.style.display='block';
-        }
-        aboutMe.onmouseout = function() {
-            ulDropdown.style.display='none';
-        }
-
-
-
-
-
-
-
-
-
-
-
+window.onload = function () {
+    var aboutMe = document.getElementById('aboutMe');
+    var ulDropdown = document.getElementById('ulDropdown');
+    aboutMe.onmouseover = function () {
+        ulDropdown.style.display = 'block';
+    }
+    aboutMe.onmouseout = function () {
+        ulDropdown.style.display = 'none';
+    }
 
 }
